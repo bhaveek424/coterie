4bc53fee735abc6a6f34a102d05a47cce0904373
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-01-08 12:07:49 +0530

    server setup

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,24 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { User } from "./entities/User";
+import express from "express";
+import morgan from "morgan";
 
-createConnection()
-  .then(async (connection) => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+const app = express();
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+//gonna use the json middleware so that i can pass json body requests
+app.use(express.json());
+app.use(morgan("dev"));
 
-    console.log("Here you can setup and run express/koa/any other framework.");
-  })
-  .catch((error) => console.log(error));
+// set up kinda a test route
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(5000, async () => {
+  console.log("Server running at http://localhost:5000");
+
+  try {
+    await createConnection();
+    console.log("Database Connected!");
+  } catch (err) {
+    console.log(err);
+  }
+});
