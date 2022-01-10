b6ff34d91050ceac57bd61923d37c6a2a8bdbdb5
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-01-10 16:43:34 +0530

    frontend register

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,21 +1,59 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex ">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Coterie: Join the community</title>
+        <link rel="icon" href="/coterie.svg" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Register{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-      </main>
+      <div
+        className="w-40 h-screen bg-center bg-cover"
+        style={{ backgroundImage: "url(/images/register-cover.png)" }}
+      ></div>
+      <div className="flex flex-col justify-center pl-6">
+        <div className="w-70">
+          <h1 className="mb-2 text-lg font-medium">Sign Up </h1>
+          <p className="mb-10 text-xs">
+            By continuing, you are setting up a Coterie account and agree to our
+            User Agreement and Privacy Policy.
+          </p>
+          <form>
+            <div className="mb-2">
+              <input
+                type="text"
+                className="w-full px-3 py-2 bg-white border border-gray-400 rounded"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-2">
+              <input
+                type="email"
+                className="w-full px-3 py-2 bg-white border border-gray-400 rounded"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-2">
+              <input
+                type="password"
+                className="w-full px-3 py-2 bg-white border border-gray-400 rounded"
+                placeholder="Password"
+              />
+            </div>
+            <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded">
+              Sign Up
+            </button>
+          </form>
+          <small>
+            Already a Coterier?
+            <Link href="/login">
+              <a className="ml-1 text-blue-500">Log In</a>
+            </Link>
+          </small>
+        </div>
+      </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        70: "16.5rem",
+      },
+    },
   },
   plugins: [],
-}
+};
